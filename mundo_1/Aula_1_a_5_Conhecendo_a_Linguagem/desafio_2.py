7d58c1fbd7f8e9f8bf619d6ad0216691bdb9ddec
eduardostr <eduardosantosrodrigues.info@gmail.com>
2025-04-24 23:42:47 -0300

    primeiro commit

---
+++ mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_2.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+
+dia = (input("Dia de nascimento: \n"))
+mes = (input("Mês de nascimento: \n"))
+ano = (input("Ano de nascimento: \n"))
+
+print("Você nasceu no dia {} de {} de {}. Correto?".format(dia, mes, ano))
---
+++ mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_3.py
@@ -0,0 +1,5 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+num1 = int(input("Primeiro número: \n"))
+num2 = int(input("Segundo número: \n"))
+print("A soma é: {}!".format(num1 + num2))
